f1319b120b025b98e062580bf54b4f33a9525289
SS201801 <ss12111991@yahoo.co.jp>
2019-08-25 17:37:04 +0900

    Initial Commit

---
+++ SayIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SayIt
+//
+//  Created by 佐々木駿 on 2019/08/25.
+//  Copyright © 2019 shun.sasaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
